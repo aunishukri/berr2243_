4454e1862d8c3851c43e3cb4dc4c221bdbbaea48
aunishukri <b022210115@student.utem.edu.my>
2024-04-17 22:57:13 +0800

    implemented

--- index.js
+++ index.js
@@ -1,12 +1,97 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
+const bcrypt = require('bcrypt');
+
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-   res.send('Hello World!')
+//new user registration
+app.post('/user', async (req, res) => {
+  // check if username already exist ??
+  let existing = await client.db("testcollectionrun").collection("datauser").findOne({
+    username: req.body.username
+  })
+
+  if (existing) {
+    res.status(400).send("username already exist")
+  } else {
+    // insertOne the registration data to mongo
+    //console.log(req.body.username)
+    const hash = bcrypt.hashSync(req.body.password, 10);
+
+    //store hash in your password DB
+    let result = await client.db("testcollectionrun").collection("datauser").insertOne(
+      {
+        username: req.body.username,
+        password: hash,
+        name: req.body.name,
+        email: req.body.email
+      }
+    )
+    res.send(result)
+  }
 })
+
+// user login api 
+app.post('/login', async (req, res) => {
+  // step #1: req.body.username ??
+  if (req.body.username != null && req.body.password != null) {
+    let result = await client.db("testcollectionrun").collection("datauser").findOne({
+      username: req.body.username
+    })
+
+    if (result) {
+      // step #2: if user exist, check if password is correct
+      //console.log(req.body.password)
+      //console.log(result.password)
+      //to check a password
+      //Load hash from your password DB
+
+      if (bcrypt.compareSync(req.body.password, result.password) == true) {
+        // password is correct
+        res.send('login successful')
+        res.send("Welcome back " + result.name)
+      } else {
+        // password is incorrect
+        res.status(401).send('wrong password')
+      }
+
+    } else {
+      // step #3: if user not found
+      res.status(401).send("username is not found")
+    }
+  } else {
+    res.status(400).send("missing username or password")
+  }
+})
+
+
+
+
+// get user profile
+app.get('/user/:siapadia/:emaildia', async (req, res) => {
+  // findOne
+  let result = await client.db('maybank2u').collection('users').findOne({
+    username: req.params.siapadia,
+    email: req.params.emaildia
+  })
+  res.send(result)
+})
+
+
+// update user account
+app.patch('/user', (req, res) => {
+  // updateOne
+  console.log('update user profile')
+})
+
+// delete user account
+app.delete('/user', (req, res) => {
+  // deleteOne
+  console.log('delete user account')
+})
+
 
 app.listen(port, () => {
    console.log(`Example app listening on port ${port}`)
@@ -37,5 +122,4 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
 
